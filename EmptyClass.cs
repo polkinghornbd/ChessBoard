bf7f7abbc787afca0c4b0f575f1a330384353d7e
Bryce Polkinghorn <polkinghornbd@gmail.com>
2017-11-03 15:15:27 -0700

    Moved Chess code to folder
    Mono made lots of little folders that I'm not too sure I understand, but I think it's okay to git them...

---
+++ EmptyClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ChessBoard
+{
+	public class GenerateBoard
+	{
+		public static void Main ()
+		{
+
+			int boardSize = 8;
+
+			for (int x = 0; x < boardSize; x++) {
+				for (int y = 0; y < boardSize; y++) {
+					if (x % 2 == 0) {
+						if (y % 2 == 0) {
+							
+							Console.Write ("[ ] ");
+
+						} else {
+							
+							Console.Write ("{ } ");
+
+
+						}
+					} else {
+
+						if (y % 2 == 0) {
+							
+							Console.Write ("{ } ");
+
+
+						} else {
+							
+							Console.Write ("[ ] ");
+
+						}
+					}
+
+				}
+
+					Console.WriteLine ("");
+			}
+
+		}
+	}
+}
+
